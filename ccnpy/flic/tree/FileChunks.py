7dbc02560fa3747d0710f91302247db888cf5b7b
Marc Mosko <marc@mosko.org>
2025-03-16 15:08:43 -0700

    fix unit test bug

--- ccnpy/flic/tree/FileChunks.py
+++ ccnpy/flic/tree/FileChunks.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
+from typing import Iterable
 
 from .SizedPointer import SizedPointer
 
@@ -43,7 +43,10 @@ class FileChunks:
     def reverse_iterator(self):
         return self.__iter__()
 
-    class ReverseIterator:
+    class ReverseIterator(Iterable):
+        def __iter__(self):
+            return self
+
         def __init__(self, iterable):
             self._iterable = iterable
             self._offset = len(iterable) - 1
